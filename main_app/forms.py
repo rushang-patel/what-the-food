6458344b5f779777046f1bf798bfbfe83cb9dec3
Rushang Patel <rushang1993@gmail.com>
2023-08-05 20:42:05 -0500

    recipe and cuttingboard CRUD redefined, andd CSS added. User login required to create, edit, and delete implemented

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,10 +1,24 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe, CuttingBoard
 
 class RecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super(RecipeForm, self).__init__(*args, **kwargs)
+
+        if self.user:
+            if not self.instance.user == self.user:
+                self.fields['optional_link'].widget = forms.HiddenInput()
+
     class Meta:
         model = Recipe
         fields = ['title', 'prep_time', 'ingredients', 'steps', 'optional_link', 'upload_file', 'checkboxes']
         widgets = {
             'checkboxes': forms.CheckboxSelectMultiple,
         }
+
+
+class CuttingBoardForm(forms.ModelForm):
+    class Meta:
+        model = CuttingBoard
+        fields = ['title', 'description']
--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
-    prep_time = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first().id)
+    title = models.CharField(max_length=100)
+    prep_time = models.CharField(max_length=50)
     ingredients = models.TextField()
     steps = models.TextField()
     optional_link = models.URLField(blank=True)
+    upload_file = models.FileField(blank=True, upload_to='uploads/')
     upload_file = models.FileField(upload_to='recipe_uploads/', blank=True)
+    cutting_boards = models.ManyToManyField('CuttingBoard', blank=True)
 
     CHECKBOX_CHOICES = (
         ('Vegetarian', 'Vegetarian'),
@@ -17,6 +21,15 @@ class Recipe(models.Model):
     )
     
     checkboxes = models.CharField(max_length=100, choices=CHECKBOX_CHOICES, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class CuttingBoard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
