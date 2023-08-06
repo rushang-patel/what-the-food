486b609e4b8d0f6060daf256ff84c5faf8f8d9b5
Rushang Patel <rushang1993@gmail.com>
2023-08-06 16:57:10 -0500

    heroku ddatabase debug

--- whatthefood/settings.py
+++ whatthefood/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +81,9 @@ WSGI_APPLICATION = 'whatthefood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Replace the existing DATABASES configuration with the following
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'whatthefood',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
